06cd9f446987d33343c85030022829ffdc0a4727
Nahin <nahin.ahmed28@gmail.com>
2023-07-26 00:25:52 +0600

    footer design updated

--- resources/views/frontend/layouts/common/footer.blade.php
+++ resources/views/frontend/layouts/common/footer.blade.php
@@ -4,21 +4,37 @@
 
             <div class="col-sm-6">
                 <div class="footer-section">
-                    <p class="copyright">Juli &copy; 2018. All rights reserved | Made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a></p>
-                </div><!-- footer-section -->
+                    <h3> Samiul Hoque </h3>
+                    <p class="text-white">
+                        Head of Administration <br>
+                        Center for Trade and Investment <br>
+                        University of Dhaka <br>
+                        Room: 9050-52, 8th Floor, MBA Building, <br>
+                        Faculty of Business Studies, <br>
+                        University of Dhaka<br>
+                        <strong>Email :</strong>  samiulhoque63@yahoo.com<br>
+                        <strong>Cell :</strong>  01717044316<br>
+                    </p>                </div><!-- footer-section -->
             </div><!-- col-lg-4 col-md-6 -->
+
 
             <div class="col-sm-6">
                 <div class="footer-section">
-                    <ul class="social-icons">
-                        <li><a href="#"><i class="ion-social-facebook-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-twitter-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-vimeo-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-pinterest-outline"></i></a></li>
+                    <h3>Useful Links</h3>
+                    <ul style="list-style: none">
+                        <li style=" display: block;"><i class="bx bx-chevron-right"></i> <a href="https://www.du.ac.bd/" target="_blank">Dhaka University</a></li>
+                        <li style=" display: block;"><i class="bx bx-chevron-right"></i> <a href="https://bangladesh.gov.bd" target="_blank">Bangladesh Government</a></li>
+                        <li style=" display: block;"><i class="bx bx-chevron-right"></i> <a href="http://www.bbs.gov.bd/" target="_blank">Bangladesh National Portal</a></li>
+                        <li style=" display: block;"><i class="bx bx-chevron-right"></i> <a href="http://www.nsda.gov.bd/" target="_blank">National Skill Development Authority</a></li>
+                        <a href="https://www.facebook.com/icgsbd" target="_blank">
+                            <li style=" margin-top: 20px; border:1px solid whitesmoke;border-radius: 25px;" class="p-3" ><i class="ion-social-facebook-outline"></i>
+                            </li>
+                        </a>
                     </ul>
                 </div><!-- footer-section -->
             </div><!-- col-lg-4 col-md-6 -->
+
+
 
         </div><!-- row -->
     </div><!-- container -->
